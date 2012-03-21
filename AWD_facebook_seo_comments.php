648429782faa879a6b5a75a84af6f7796df25c23
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-21 03:32:59 +0000

    Fix deps
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd-seo-comments/trunk@521668 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- AWD_facebook_seo_comments.php
+++ AWD_facebook_seo_comments.php
@@ -3,7 +3,7 @@
 Plugin Name: Facebook AWD Seo Comments
 Plugin URI: http://www.ahwebdev.fr
 Description: This plugin merge Facebook Comments with native Wordpress Comments System. Need Facebook AWD All in One plugin.
-Version: 1.1
+Version: 1.2
 Author: AHWEBDEV
 Author URI: http://www.ahwebdev.fr
 License: AHWEBDEV
@@ -15,10 +15,12 @@ add_action('plugins_loaded', 'initial_seo_comments');
 function initial_seo_comments()
 {
 	global $AWD_facebook;
-	$model_path = $AWD_facebook->get_plugins_model_path();
-	require_once($model_path);
-	require_once(dirname(__FILE__).'/inc/classes/class.AWD_facebook_seo_comments.php');
-	//instance
-	$AWD_facebook_seo_comments = new AWD_facebook_seo_comments(__FILE__,$AWD_facebook);
+	if(is_object($AWD_facebook)){
+		$model_path = $AWD_facebook->get_plugins_model_path();
+		require_once($model_path);
+		require_once(dirname(__FILE__).'/inc/classes/class.AWD_facebook_seo_comments.php');
+		//instance
+		$AWD_facebook_seo_comments = new AWD_facebook_seo_comments(__FILE__,$AWD_facebook);
+	}
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: Facebook comments, seo comments, facebook awd, facebook
 Requires at least: 3.3
 Tested up to: 3.3.1
-Stable tag: 1.1
+Stable tag: 1.2
 
 This plugin merge Facebook Comments with native Wordpress Comments System. Need Facebook AWD All in One plugin.
 

886ab41f0194d98b3a3a275c8464806688448b6d
AHWEBDEV <AHWEBDEV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-02 23:14:30 +0000

    fix error url of comments
    git-svn-id: https://plugins.svn.wordpress.org/facebook-awd-seo-comments/trunk@633229 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/classes/class.AWD_facebook_comments_base.php
+++ inc/classes/class.AWD_facebook_comments_base.php
@@ -76,7 +76,7 @@ class AWD_facebook_comments_base
 	{
 		if($this->comments_url == '') 
 			return false;
-		$fql = "SELECT comments_fbid,commentsbox_count,comment_count FROM link_stat WHERE url='http://www.ahwebdev.fr/plugins/facebook-awd.html'";
+		$fql = "SELECT comments_fbid,commentsbox_count,comment_count FROM link_stat WHERE url='".$this->comments_url."'";
 		try {
 			$fql_url_object = $this->AWD_facebook->fcbk->api(array('method'=>'fql.query','query'=>$fql));
 			$this->comments_count = 0;
